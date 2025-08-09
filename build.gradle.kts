4b96b1b70125ee6979f2673a601d760926ce799d
ricflima <111242248+ricflima@users.noreply.github.com>
2025-08-09 20:53:48 -0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-
+    id("com.android.application") version "8.0.0" apply false // Use sua versão do Android Gradle Plugin
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false // Adicione esta linha
+    id("com.google.dagger.hilt.android") version "2.48" apply false // Exemplo, use sua versão do Hilt
+    // ...
 }
